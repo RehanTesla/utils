8fd2a242b9f96e59b944a12140dfb6ecf9e1df3f
RehanTesla <rehantesla04@gmail.com>
2025-09-17 13:29:30 +0700

    Create ToPlainObject.js

---
+++ src/ToPlainObject.js
@@ -0,0 +1,30 @@
+import isObject from "./IsObject";
+import isBoolean from "./IsBoolean.js";
+const toPlainObject = (obj, andArray, arrMap) => {
+    if (isObject(obj)) return obj;
+    if (!isBoolean(andArray)) {
+        andArray = true;
+    }
+    if (Array.isArray(arrMap)) {
+        andArray = true;
+    }
+    let plain;
+    if (Array.isArray(obj)) {
+        if (andArray) {
+            plain = obj.map(toPlainObjectInPlace);
+        }
+    } else {
+        plain = {};
+        for (const key of Object.keys(obj)) {
+            plain[key] = toPlainObjectInPlace(obj[key]);
+        }
+    }
+    for (const key of Object.keys(obj)) {
+        delete obj[key];
+    }
+    Object.assign(obj, plain);
+    Object.setPrototypeOf(obj, Object.prototype);
+    return obj;
+};
+toPlainObject.fileURL = import.meta.url;
+export { toPlainObject as default, toPlainObject };
