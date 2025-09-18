ccf7b85559fb4cb43ea8d88155669d61bca8fbb0
RehanTesla <rehantesla04@gmail.com>
2025-09-18 14:32:31 +0700

    Update ParseString.js

--- src/ParseString.js
+++ src/ParseString.js
@@ -1,9 +1,9 @@
 import isString from "./IsString.js";
 const parseString = arg => {
-  if (!isString(arg) {
-    arg = "";
-  };
-  return arg;
+    if (!isString(arg)) {
+        arg = "";
+    }
+    return arg;
 };
 parseString.fileURL = import.meta.url;
 export { parseString as default, parseString };
